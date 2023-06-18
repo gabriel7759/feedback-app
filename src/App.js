70beb447507926ce2d8de253fb82704a512229a7
Gabriel Velazquez <gabriel7759@hotmail.com>
2023-06-18 16:28:10 -0700

    Basic Css

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Header from "./components/Header";
 function App() {
     return (
         <div className="App">
-            <Header text="Hell World" />
+            <Header text="Feedback UI" />
             <h1>My App</h1>
         </div>
     );
