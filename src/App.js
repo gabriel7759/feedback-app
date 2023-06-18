3517f5ccfe4ecf3a705463f5d043e7b8211a664a
Gabriel Velazquez <gabriel7759@hotmail.com>
2023-06-18 15:40:54 -0700

    Display data from variables

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
 function App() {
+    const title = 'My App';
+    const body = 'This is my blog post';
+
+    const comments = [
+        {id: 1, text:'Comment one'},
+        {id: 2, text:'Comment two'},
+        {id: 3, text:'Comment three'},
+    ];
+
     return (
         <div className="App">
-            <h1>My App</h1>
+            <h1>{title}</h1>
+            <p>{body}</p>
+
+            <div className="comments">
+                <h3>Comments ({comments.length})</h3>
+                <ul>
+                    {comments.map((row, index) => (
+                        <li key={index}>{row.text}</li>
+                    ))}
+                </ul>
+            </div>
         </div>
     );
 }
