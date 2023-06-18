1f0a394947da994ce1a55137cf368a7124e26ea8
Gabriel Velazquez <gabriel7759@hotmail.com>
2023-06-18 15:56:14 -0700

    Create a basic component and send props

--- src/App.js
+++ src/App.js
@@ -1,26 +1,10 @@
+import Header from "./components/Header";
+
 function App() {
-    const title = 'My App';
-    const body = 'This is my blog post';
-
-    const comments = [
-        {id: 1, text:'Comment one'},
-        {id: 2, text:'Comment two'},
-        {id: 3, text:'Comment three'},
-    ];
-
     return (
         <div className="App">
-            <h1>{title}</h1>
-            <p>{body}</p>
-
-            <div className="comments">
-                <h3>Comments ({comments.length})</h3>
-                <ul>
-                    {comments.map((row, index) => (
-                        <li key={index}>{row.text}</li>
-                    ))}
-                </ul>
-            </div>
+            <Header text="Hell World" />
+            <h1>My App</h1>
         </div>
     );
 }
