396aa93af7460843f002e956dac2681887a1a7da
Gabriel Velazquez <gabriel7759@hotmail.com>
2023-06-18 17:07:40 -0700

    Component to display ietms list read from json value

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
+import { useState } from "react";
 import Header from "./components/Header";
-import FeedbackItem from "./components/FeedbackItem";
+import FeedbackList from "./components/FeedbackList";
+import FeedbackData from "./data/FeedbackData.js";
 
 function App() {
+    const [feedback, setFeedback] = useState(FeedbackData);
+
     return (
         <div className="App">
             <Header text="Feedback UI" />
-            <FeedbackItem />
+            <FeedbackList feedback={feedback} />
         </div>
     );
 }
