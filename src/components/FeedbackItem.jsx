8239d601d7e386c90f2977444ac71b399c29c457
Gabriel Velazquez <gabriel7759@hotmail.com>
2023-06-18 19:26:45 -0700

    Font Awsome icons

--- src/components/FeedbackItem.jsx
+++ src/components/FeedbackItem.jsx
@@ -1,12 +1,21 @@
+import { FaTimes } from "react-icons/fa"
+import { PropTypes } from "prop-types";
 import Card from "./shared/Card";
 
 function FeedbackItem({item}) {
   return (
     <Card>
         <div className="num-display">{item.rating}</div>
+        <button className="close">
+          <FaTimes color="purple" />
+        </button>
         <div className="text-display">{item.text}</div>
     </Card>
-  )
+  );
 }
 
-export default FeedbackItem
+FeedbackItem.propTypes = {
+  item: PropTypes.object.isRequired,
+};
+
+export default FeedbackItem;
