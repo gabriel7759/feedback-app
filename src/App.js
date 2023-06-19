04918fc5e37333b521f9b6846cfc6deedaa78e22
Gabriel Velazquez <gabriel7759@hotmail.com>
2023-06-18 19:44:16 -0700

    Prop Drilling and mock delete functionality

--- src/App.js
+++ src/App.js
@@ -6,10 +6,16 @@ import FeedbackData from "./data/FeedbackData.js";
 function App() {
     const [feedback, setFeedback] = useState(FeedbackData);
 
+    const deleteFeedback = (id) => {
+        if(window.confirm('Are you sure you want to delete the item?')) {
+            setFeedback(feedback.filter((row) => row.id !== id));
+        }
+    };
+
     return (
         <div className="App">
             <Header text="Feedback UI" />
-            <FeedbackList feedback={feedback} />
+            <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
         </div>
     );
 }
--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -1,24 +1,25 @@
 import { PropTypes } from "prop-types";
 import FeedbackItem from "./FeedbackItem";
 
-function FeedbackList({ feedback}) {
+function FeedbackList({ feedback, handleDelete }) {
+  if (!feedback || feedback.length === 0) {
+    return <p>No Feedback Yet</p>;
+  }
 
-    if(!feedback || feedback.length === 0) {
-        return <p>No Feedback Yet</p>
-    }
-
-    return (
-        <div className="feedback-list">
-            {feedback.map((row) => (
-                <FeedbackItem key={row.id} item={row} />
-            ))}
-        </div>
-    );
+  return (
+    <div className="feedback-list">
+      {feedback.map((row) => (
+        <FeedbackItem 
+            key={row.id} 
+            item={row}
+            handleDelete={handleDelete} />
+      ))}
+    </div>
+  );
 }
 
 FeedbackList.propTypes = {
-    feedback: PropTypes.array,
+  feedback: PropTypes.array,
 };
-
 
 export default FeedbackList;
