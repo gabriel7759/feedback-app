4570bef11fe2ab60f906b5995e411e8d41e571d5
Gabriel Velazquez <gabriel7759@hotmail.com>
2023-06-22 20:32:00 -0700

    Add feedback functionality

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { v4 as uuidv4 } from "uuid";
 import { useState } from "react";
 import Header from "./components/Header";
 import FeedbackList from "./components/FeedbackList";
@@ -8,8 +9,13 @@ import FeedbackData from "./data/FeedbackData.js";
 function App() {
     const [feedback, setFeedback] = useState(FeedbackData);
 
+    const addFeedback = (newFeedback) => {
+        newFeedback.id = uuidv4();
+        setFeedback([newFeedback, ...feedback]);
+    };
+
     const deleteFeedback = (id) => {
-        if(window.confirm('Are you sure you want to delete the item?')) {
+        if (window.confirm('Are you sure you want to delete the item?')) {
             setFeedback(feedback.filter((row) => row.id !== id));
         }
     };
@@ -18,8 +24,8 @@ function App() {
         <div className="App">
             <Header text="Feedback UI" />
             <div className="container">
-                <FeedbackForm feedback={feedback} />
-                <FeedbackStats feedback={feedback} /> 
+                <FeedbackForm handleAdd={addFeedback} />
+                <FeedbackStats feedback={feedback} />
                 <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
             </div>
         </div>
