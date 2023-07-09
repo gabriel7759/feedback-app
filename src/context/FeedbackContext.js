99a61efeb454a2fd0eedb380b7be096665c0b08c
Gabriel Velazquez <gabriel7759@hotmail.com>
2023-07-08 21:07:02 -0700

    Add into the JSON server

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -1,5 +1,4 @@
 import { createContext, useState, useEffect } from "react";
-import { v4 as uuidv4 } from "uuid";
 
 const FeedbackContext = createContext();
 
@@ -23,9 +22,20 @@ export const FeedbackProvider = ({children}) => {
         setIsLoading(false);
     }
 
-    const addFeedback = (newFeedback) => {
-        newFeedback.id = uuidv4();
-        setFeedback([newFeedback, ...feedback]);
+    const addFeedback = async (newFeedback) => {
+    console.log(newFeedback);
+
+        const response = await fetch("http://localhost:5000/feedback", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(newFeedback)
+        });
+
+        const data = await response.json();
+
+        setFeedback([data, ...feedback]);
     };
 
     const editFeedback = (item) => {
